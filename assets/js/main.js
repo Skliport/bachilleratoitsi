61632ecaad0666b7857a3db8e4283af9455cd78a
Julio Canizalez <juliocanizalez@outlook.com>
2022-05-23 23:23:20 -0600

    Add scripts

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,14 +30,51 @@ const i200 = {
 };
 
 //=================================
-// NAVBAR BACKGROUND ON SCROLL
+// MANAGE ACTIVE CLASS FOR NAVBAR
+//=================================
+const sections = document.getElementsByClassName('section-page');
+
+const manageActiveClass = () => {
+    const position = document.documentElement.scrollTop;
+    for (let i = 0; i < sections.length; i++) {
+        if (
+            position >=
+                sections.item(i).offsetTop -
+                    sections.item(i).offsetHeight * 0.5 &&
+            position <
+                sections.item(i).offsetTop +
+                    sections.item(i).offsetHeight -
+                    sections.item(i).offsetHeight * 0.5
+        ) {
+            const currentID = sections.item(i).id;
+
+            const navLinks = document.getElementsByClassName('nav-link');
+
+            for (let j = 0; j < navLinks.length; j++) {
+                navLinks.item(j).classList.remove('active');
+            }
+
+            document
+                .querySelector(`a[href="#${currentID}"]`)
+                .classList.add('active');
+        }
+    }
+};
+
+//=================================
+// NAVBAR BACKGROUND AND ACTIVE CLASS ON SCROLL
 //=================================
 onscroll = () => {
+    manageActiveClass();
     const nav = document.getElementById('navbar');
 
     window.scrollY > 0
         ? nav.classList.add('bg-dark')
         : nav.classList.remove('bg-dark');
+};
+
+onload = () => {
+    manageActiveClass();
 };
 
 //=================================
